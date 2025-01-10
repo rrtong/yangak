f0eeea665a1aee481d184fd3d0a1a6d0d1471a9d
rrTong <rrtong@ucdavis.edu>
2025-01-09 18:18:07 -0800

    first commit -- cra

---
+++ README.md
@@ -0,0 +1,3 @@
+# yangak
+
+Personality Quizzes
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>yangak</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
